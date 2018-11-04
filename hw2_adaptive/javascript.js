448728dcd01d34ed7f9c44fcaab9ea270eff8054
Valentina Pechonchik <valentina.pechonchik@gmail.com>
2018-11-04 23:08:27 +0200

    hw2

---
+++ hw2_adaptive/javascript.js
@@ -0,0 +1,28 @@
+// let nav = document.getElementsByClassName("nav");
+// let nav_menu = document.getElementsByClassName("nav_menu");
+// let fa_bars = document.getElementsByClassName("fa-bars");
+// let fa_times = document.getElementsByClassName("fa-times");
+// let smallScreen = window.matchMedia("(max-width: 980px)");
+
+
+// function showMenu() {
+//     if (smallScreen.matches) {
+//         fa_bars[0].style.display = "none";
+//         fa_times[0].style.display = "block";
+//         fa_times[0].style.marginRight = "17px";
+//         fa_times[0].style.marginLeft = "18px";
+//         // nav[0].style.display = "block";
+//     }
+// }
+
+// function hideMenu() {
+//     if (smallScreen.matches) {
+//         fa_bars[0].style.display = "block";
+//         fa_times[0].style.display = "none";
+//         fa_bars[0].style.marginRight = "17px";
+//         fa_bars[0].style.marginLeft = "18px";
+//         nav[0].style.display = "none";
+//     }
+
+// }
+
