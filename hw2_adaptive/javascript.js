06057b0a8355b91c4a4f5447e511936ab038865a
Valentina Pechonchik <valentina.pechonchik@gmail.com>
2018-11-06 23:42:03 +0200

    click on burger menu added

--- hw2_adaptive/javascript.js
+++ hw2_adaptive/javascript.js
@@ -1,28 +1,22 @@
-// let nav = document.getElementsByClassName("nav");
-// let nav_menu = document.getElementsByClassName("nav_menu");
-// let fa_bars = document.getElementsByClassName("fa-bars");
-// let fa_times = document.getElementsByClassName("fa-times");
+let nav = document.getElementsByClassName("nav");
+let nav_menu = document.getElementsByClassName("nav_menu");
+let fa_bars = document.getElementsByClassName("fa-bars");
+let fa_times = document.getElementsByClassName("fa-times");
 // let smallScreen = window.matchMedia("(max-width: 980px)");
 
-
-// function showMenu() {
-//     if (smallScreen.matches) {
-//         fa_bars[0].style.display = "none";
-//         fa_times[0].style.display = "block";
-//         fa_times[0].style.marginRight = "17px";
-//         fa_times[0].style.marginLeft = "18px";
-//         // nav[0].style.display = "block";
-//     }
-// }
-
-// function hideMenu() {
-//     if (smallScreen.matches) {
-//         fa_bars[0].style.display = "block";
-//         fa_times[0].style.display = "none";
-//         fa_bars[0].style.marginRight = "17px";
-//         fa_bars[0].style.marginLeft = "18px";
-//         nav[0].style.display = "none";
-//     }
-
+fa_bars[0].onclick = function(){
+    fa_bars[0].style.display = "none";
+    fa_times[0].style.display = "block";
+    nav[0].style.display = "block";
+}
+fa_times[0].onclick = function () {
+    fa_bars[0].style.display = "block";
+    fa_times[0].style.display = "none";
+    nav[0].style.display = "none";
+}
+// nav[0].onmouseleave = function(){
+//     nav[0].style.display = "none";
+//     fa_bars[0].style.display = "block";
+//     fa_times[0].style.display = "none";
 // }
 
